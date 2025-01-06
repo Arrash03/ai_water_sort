ebf4c8a72a07824932941ba306819c5f9ba9451b
Arrash Tahery <atv1382@gmail.com>
2025-01-06 15:05:13 +0330

    solve added successfully

--- ai_solution.py
+++ ai_solution.py
@@ -1,4 +1,12 @@
+import copy
+from typing import List
+
+
+Tube = List[int]
+MotherTube = List[Tube]
+
 class GameSolution:
+    MAX_DEPTH: int = 10
     """
         A class for solving the Water Sort game and finding solutions(normal, optimal).
 
@@ -28,7 +36,50 @@ class GameSolution:
         self.solution_found = False  # True if a solution is found, False otherwise.
         self.visited_tubes = set()  # A set of visited tubes.
 
-    def solve(self, current_state):
+    def __is_tube_completed(self, tube: Tube) -> bool:
+        result = False
+        if len(tube) == self.ws_game.NColorInTube:
+            tmp_set = set(tube)
+            if len(tmp_set) == 1:
+                result = True
+        return result
+
+    def __choose_source(self, tubes: MotherTube) -> List[int]:
+        MIN_TUBE_LEN: int = 0
+        sources: List[int] = []
+        for i in range(len(tubes)):
+            if len(tubes[i]) != MIN_TUBE_LEN and not self.__is_tube_completed(tubes[i]):
+                sources.append(i)
+        return sources
+
+    def __choose_destination(self, tubes: MotherTube) -> List[int]:
+        destinations: List[int] = []
+        for i in range(len(tubes)):
+            if len(tubes[i]) != self.ws_game.NColorInTube:
+                destinations.append(i)
+        return destinations
+
+    @staticmethod
+    def __move_from_src_to_dst(tubes: MotherTube, src: int, dst: int) -> None:
+        tubes[dst].append(tubes[src][-1])
+        tubes[src].pop()
+
+    def __check_win(self, tubes: MotherTube) -> bool:
+        result = True
+        if len(tubes) == 0:
+            result = False
+        else:
+            for tube in tubes:
+                if len(tube) != 0 and len(tube) != self.ws_game.NColorInTube:
+                    result = False
+                    break
+                tmp_set = set(tube)
+                if len(tmp_set) > 1:
+                    result = False
+                    break
+        return result
+
+    def solve(self, current_state: MotherTube, moves: List[List[int]] = [], depth: int = MAX_DEPTH) -> bool:
         """
             Find a solution to the Water Sort game from the current state.
 
@@ -38,7 +89,33 @@ class GameSolution:
             This method attempts to find a solution to the Water Sort game by iteratively exploring
             different moves and configurations starting from the current state.
         """
-        pass
+        if depth == 0:
+            return False
+        
+        while True:
+            sources = self.__choose_source(current_state)
+            destinations = self.__choose_destination(current_state)
+            for src in sources:
+                for dst in destinations:
+                    if src == dst:
+                        continue
+                    own_current_state = copy.deepcopy(current_state)
+                    GameSolution.__move_from_src_to_dst(own_current_state, src, dst)
+                    own_moves = copy.deepcopy(moves)
+                    own_moves.append([src, dst])
+                    if self.__check_win(own_current_state):
+                        self.solution_found = True
+                        self.moves = own_moves
+                        return True 
+                    result = self.solve(own_current_state, own_moves, depth - 1)
+                    if result:
+                        return True
+
+            if depth == GameSolution.MAX_DEPTH:
+                depth *= 2
+            else:
+                break
+        print("a")
 
     def optimal_solve(self, current_state):
         """
